9a3561e18f4ca666d6e97db257457ef8ba4f39b0
MatomsPapa <obadeyiolayinka@gmail.com>
2023-10-19 07:24:54 -0700

    converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,12 @@
 unsigned int binary_to_uint(const char *b)
 {
 	if (b == NULL)
-	{
-		return (0);
-	}
+	return 0;
+       
 	unsigned int dec_val = 0;
-
-	for (int i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-		{
-			return (0);
-		}
+	for (int i = 0; b[i] != '\0'; i++) {
+		if (b[i] < '0' || b[i] > '1')
+			return 0;
 		dec_val = (dec_val << 1) | (b[i] - '0');
 	}
 	return (dec_val);
